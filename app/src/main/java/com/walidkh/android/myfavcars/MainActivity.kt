f3e957593add324eec49d3258fa6d4f70e0609f9
walid <walid.khalladi@hotmail.com>
2020-12-15 09:35:01 +0100

    add navigation

--- app/src/main/java/com/walidkh/android/myfavcars/MainActivity.kt
+++ app/src/main/java/com/walidkh/android/myfavcars/MainActivity.kt
@@ -2,10 +2,30 @@ package com.walidkh.android.myfavcars
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.walidkh.android.myfavcars.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = DataBindingUtil
+            .setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.main_nav_host) as NavHostFragment
+        val navController = navHostFragment.navController
+        navController.addOnDestinationChangedListener {_, destination, _ ->
+            when(destination.id) {
+                R.id.loginFragment -> binding.appBar.visibility = GONE
+                else -> binding.appBar.visibility = VISIBLE
+            }
+        }
     }
 }
