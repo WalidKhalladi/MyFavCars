5b02071a05cafe2616c8fbbf4ce3af3d4563cc77
walid <walid.khalladi@hotmail.com>
2020-12-17 20:15:49 +0100

    add cars list viewModel

--- app/src/main/java/com/walidkh/android/myfavcars/cars/CarsListViewModel.kt
+++ app/src/main/java/com/walidkh/android/myfavcars/cars/CarsListViewModel.kt
@@ -1,2 +1,27 @@
 package com.walidkh.android.myfavcars.cars
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.walidkh.android.myfavcars.models.Car
+
+class CarsListViewModel : ViewModel() {
+
+    var car : Car = Car()
+    private val _carsListData = MutableLiveData<Car>()
+    val carsListData : LiveData<Car>
+        get() = _carsListData
+
+    private val _navigateToAddPage = MutableLiveData<Boolean>()
+    val navigateToAddPage : LiveData<Boolean>
+        get() = _navigateToAddPage
+
+    fun navigateToAddPage() {
+        _navigateToAddPage.value = true
+    }
+
+    fun navigateToAddPageComplete() {
+        _navigateToAddPage.value = false
+    }
+
+}
